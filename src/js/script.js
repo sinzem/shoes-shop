215b59775a5e2531e87b789c605a484bd2ffe3a4
Sinzem <sinzem@gmail.com>
2023-03-11 20:18:31 +0200

    mobile-top-and-slider

--- src/js/script.js
+++ src/js/script.js
@@ -35,21 +35,4 @@ function documentActions(e) {
     if (targetElement.closest('.icon-menu')) {
         document.documentElement.classList.toggle('menu-open');
     }
-
-    if (targetElement.closest('[data-goto]')) {
-        document.documentElement.classList.contains('menu-open') ?
-            document.documentElement.classList.remove('menu-open') : null;
-            
-        const goTo = targetElement.closest('[data-goto]').dataset.goto;
-        const goToElement = document.querySelector(goTo);
-        const headerHeight = document.querySelector('.header').offsetHeight;
-
-        if (goToElement) {
-            window.scrollTo({
-                top: goToElement.offsetTop - (headerHeight + 15),
-                behavior: "smooth"
-            });
-        }
-        e.preventDefault();
-    }
 }
